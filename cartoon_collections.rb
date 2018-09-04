1243dde125bcb5fdc6c942289601c3072e63bde3
cd <coraldarling@outlook.com>
2018-09-04 20:11:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find? do |snack| 
-    cheese_types.include?(snack)
+    cheese_types.include?("snack")
     end
   end
 end
